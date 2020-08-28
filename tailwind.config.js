7bbe754a7041edde965afce30a4df0e73e3542c8
hardian-n <hardian.nurhadi@gmail.com>
2020-08-28 23:44:19 +0700

    init

---
+++ tailwind.config.js
@@ -0,0 +1,35 @@
+module.exports = {
+  purge: [],
+  theme: {
+    spacing: {
+      px: '1px',
+      '0': '0',
+      '1': '0.25rem',
+      '2': '0.5rem',
+      '3': '0.75rem',
+      '4': '1rem',
+      '5': '1.25rem',
+      '6': '1.5rem',
+      '8': '2rem',
+      '10': '2.5rem',
+      '12': '3rem',
+      '16': '4rem',
+      '20': '5rem',
+      '24': '6rem',
+      '32': '8rem',
+      '40': '10rem',
+      '48': '12rem',
+      '56': '14rem',
+      '64': '16rem',
+      '60p': '60px',
+    },
+    extend: {
+      colors: {
+        'biru-vercel': '#0070f3',
+        'hitam-200': '#f0f0f0',
+      }
+    },
+  },
+  variants: {},
+  plugins: [],
+}
